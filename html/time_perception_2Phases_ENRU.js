0a3bf399f757f710e4ce4a582bb88784c4ab7463
ali adeli <aliadelik@gmail.com>
2021-03-24 18:24:24 +0000

    Update time_perception_2Phases_ENRU.js

--- html/time_perception_2Phases_ENRU.js
+++ html/time_perception_2Phases_ENRU.js
@@ -411,13 +411,13 @@ function Choose_LanguageRoutineEnd(snapshot) {
     Lang_Select.stop();
     if ((Lang_Select.keys === "1")) {
         WelcomeText = WelcomeRUS;
-        RespImage = "img\"\\\"resp_longer_rus.png";
+        RespImage = "img/resp_longer_rus.png";
         RespImage = RespImage.replace("\"", "");
         PressSpace = "\u041d\u0430\u0436\u043c\u0438\u0442\u0435 '\u041f\u0440\u043e\u0431\u0435\u043b' \u0434\u043b\u044f \u043f\u0440\u043e\u0434\u043e\u043b\u0436\u0435\u043d\u0438\u044f.";
         ThankYou = "\u0421\u043f\u0430\u0441\u0438\u0431\u043e \u0431\u043e\u043b\u044c\u0448\u043e\u0435 \u0437\u0430 \u0443\u0447\u0430\u0441\u0442\u0438\u0435!";
     } else {
         WelcomeText = WelcomeENG;
-        RespImage = "img\"\\\"resp_longer.png";
+        RespImage = "img/resp_longer.png";
         RespImage = RespImage.replace("\"", "");
         PressSpace = "Press SPACE to initiate a new trial";
         ThankYou = "Thank you!";
